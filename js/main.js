9b9c9e7864c77834d6d683e165022bad682c873d
Siu Kei Tam <sktam@sius-air.home>
2020-04-18 12:50:33 +0100

    Added navBar

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const navSlide = () => {
+    const navBar = document.querySelector('.toggle-collapse');
+    const navItems = document.querySelector('.nav-items');
+    const navLinks = document.querySelectorAll('.nav-items .nav-link');
+
+
+
+    navBar.addEventListener('click', () => {
+        
+        //Toggle Nav        
+        navItems.classList.toggle('nav-active');
+
+        //Animate links    
+        navLinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = '';
+            } else{
+            link.style.animation = `navItemsFade 0.5s ease forwards ${index/7 + 1}s`
+            }
+        });
+
+
+        //Navbar animation
+        navBar.classList.toggle('navBar-active');
+    })
+
+
+
+
+
+}
+
+navSlide();
